89fb54228afb4075cd8ab740dc20f470cb7846c3
abdulazizkurniawan <48430445+abdulazizkurniawan@users.noreply.github.com>
2019-04-21 22:22:24 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=hqJjjSNazBjB3qb5KkbYHWCRpX9QlNCELGrS5wCx";
         
         $("#loadImage").click(function(){
             $.ajax({
